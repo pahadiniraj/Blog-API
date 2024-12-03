b9a6601f62c5ec2ce93c87ad853b5f7facfc009a
pahadiniraj <sharma12345niraj@gmail.com>
2024-12-03 21:26:13 +0545

    prisma added

---
+++ index.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import http from "http";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  express.json({
+    limit: "20kb",
+  })
+);
+const PORT = 8000;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: (origin, callback) => {
+      const allowedOrigins = ["http://localhost:3000"];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
+app.use(compression());
+
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("Hello, World i'm niraj!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running in port ${PORT}`);
+});
